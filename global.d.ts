32998cebb87aa5ef38bd1c579c42b0be79bce6cd
张省会 <shenghui.zhang@17zuoye.com>
2023-05-05 17:22:22 +0800

    wip: 格式化

--- global.d.ts
+++ global.d.ts
@@ -12,7 +12,7 @@ declare module '*.sass';
 declare module '*.styl';
 
 declare namespace NodeJS {
-  interface ProcessEnv {
-    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq' | 'jd'
-  }
+    interface ProcessEnv {
+        TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq' | 'jd';
+    }
 }
---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+    printWidth: 1000, // 单行代码字数
+    trailingComma: 'es5', // 尾逗号
+    tabWidth: 4,
+    semi: true, // 分号
+    singleQuote: true, // 单引号替换双引号
+    vueIndentScriptAndStyle: false, // vue中的style和script最外层indent
+    // endOfLine: 'lf', // 行尾格式
+    quoteProps: 'consistent', // 对象内的属性名，根据需要统一是否加引号
+    arrowParens: 'avoid', // (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号
+};
